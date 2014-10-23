ed702eee98f8258353610966e387b8ebf6298a98
Freyr Bergsteinsson <fresnik@gmail.com>
2014-10-23 21:25:18 +0000

    Adding javascript schenanigans to the web frontend

---
+++ www/js/mylla.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#game li').click(function(){
+        alert('You clicked a button!');
+        // TODO: Send an AJAX request requesting a play for the current button
+    });
+});
