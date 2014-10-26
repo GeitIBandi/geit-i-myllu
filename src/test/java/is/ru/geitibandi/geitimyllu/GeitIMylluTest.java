0bb436bf4fe8af7ecb4ccacb30ecb7ca18ee72c6
Helgi Bjornsson <helgi@i-c43143a5.greenqloud.internal>
2014-10-26 23:07:22 +0000

    Unit tests

--- src/test/java/is/ru/geitibandi/geitimyllu/GeitIMylluTest.java
+++ src/test/java/is/ru/geitibandi/geitimyllu/GeitIMylluTest.java
@@ -2,12 +2,15 @@ package is.ru.geitibandi.geitimyllu;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
+import org.junit.Before;
 
 // Unit tests for the class GeitIMyllu
 public class GeitIMylluTest {
 
-    GeitIMyllu game;
+    private GeitIMyllu game;
 
     @Before
     public void setUp() {
@@ -15,12 +18,188 @@ public class GeitIMylluTest {
     }
 
     @Test
-    public void someTest(){
-        assertTrue(false, "Not implemented");
+    public void gameNotNull(){
+	assertNotNull(game);
     }
 
     @Test
-    public void someOtherTest(){
-        assertTrue(false, "Not implemented");
+    public void gameStateWhenNothingHasHappend(){
+        final int value = 1;
+	int result = game.getGameState();
+	assertEquals(value, result);
+    }
+
+    @Test
+    public void boardInitialzeTest(){
+	final String value = "---------";
+	String result = game.boardAsString();
+	assertEquals(value, result);
+    }
+
+    @Test
+    public void getCellFeatureTest(){
+	final char value = '-';
+	char result = game.getCell(0,0);
+	assertEquals(value, result);
+    }
+
+    @Test
+    public void playExceptionIfOutOfBoard(){
+	boolean thrown = false;
+	try {
+	    game.play(1,4);
+	} catch (IllegalArgumentException e) {
+	    thrown = true;
+	}
+	assertTrue(thrown);
+    }
+
+    @Test
+    public void playExceptionIfCellOccupied(){
+	boolean thrown = false;
+	game.board[0][0] = 'X';
+	try {
+	    game.play(0,0);
+	} catch (IllegalArgumentException e) {
+	    thrown = true;
+	}
+	assertTrue(thrown);
+    }
+
+    @Test
+    public void gameStateAfterOneTurn() {
+	game.play(1,1);
+	assertEquals(2, game.getGameState());
+    }
+
+    @Test
+    public void gameStateAfterTwoTurns() {
+	game.play(0,0);
+	game.play(1,1);
+	assertEquals(1, game.getGameState());
+    }
+
+    @Test
+    public void gameOverRow() {
+	game.board[1][0] = 'X';
+	game.board[1][1] = 'X';
+	game.board[1][2] = 'X';
+	assertTrue(game.checkRowsForWin());
+    }
+
+    @Test
+    public void gameOverColum() {
+	game.board[0][1] = 'O';
+	game.board[1][1] = 'O';
+	game.board[2][1] = 'O';
+	assertTrue(game.checkColsForWin());
+    }
+
+    @Test
+    public void gameOverDiag() {
+	game.board[0][0] = 'X';
+	game.board[1][1] = 'X';
+	game.board[2][2] = 'X';
+	assertTrue(game.checkDiagForWin());
+    }
+
+    @Test
+    public void gameOverTest() {
+	game.board[0][0] = 'X';
+	game.board[1][1] = 'X';
+	game.board[2][2] = 'X';
+	assertTrue(game.gameWon());
+    }
+    @Test
+    public void boardFullTest() {
+	for(int i = 0; i < 3; i++){
+	    for(int j = 0; j < 3; j++){
+		game.board[i][j] = 'X';
+	    }
+	}
+	assertTrue(game.boardFull());
+    }
+
+    @Test
+    public void playerMarkIfGameStateIs1(){
+	game.setGameState(1);
+	final char value = 'X';
+	char result = game.playerMark();
+	assertEquals(value, result);
+    }
+
+    @Test
+    public void playerMarkIfGameStateIs2(){
+	game.setGameState(2);
+	assertEquals('O', game.playerMark());
+    }
+
+    @Test
+    public void playerMarkIfGameStateIs5(){
+	game.setGameState(5);
+	assertEquals('-', game.playerMark());
+    }
+
+    @Test
+    public void gameReset(){
+	game.play(2,1);
+	game.play(1,1);
+	game.resetGame();
+	assertEquals('-', game.board[1][1]);
+    }
+
+    @Test
+    public void testGetTurnFunctionAfterOneTurn(){
+	game.play(1,1);
+	assertEquals(1, game.getTurn());
+    }
+
+    @Test
+    public void testGetCurrentPlayerFunction(){
+	assertEquals('X', game.getCurrentPlayer());
+    }
+
+    @Test
+    public void testGetPlayedGamesFunctionAfterOneReset(){
+	game.resetGame();
+	assertEquals(1, game.getPlayedGames());
+    }
+
+    @Test
+    public void testGamesWonByPlayerXAfterOneWin(){
+	game.play(0,0);
+	game.play(1,0);
+	game.play(0,1);
+	game.play(1,1);
+	game.play(0,2);
+	assertEquals(1, game.getGamesWonByPlayerX());
+    }
+
+    @Test
+    public void testGamesWonByPlayerOAfterOneTie(){
+	game.play(0,0);
+	game.play(0,1);
+	game.play(0,2);
+	game.play(1,1);
+	game.play(1,0);
+	game.play(2,0);
+	game.play(1,2);
+	game.play(2,2);
+	game.play(2,1);
+	assertEquals(0, game.getGamesWonByPlayerO());
+    }
+
+    @Test
+    public void testGameTiesAfterOneTie(){
+	game.play(0,0);
+	game.play(0,1);
+	game.play(0,2);
+	game.play(1,1);
+	game.play(1,0);
+	game.play(2,0);
+	game.play(1,2);
+	game.play(2,2);
+	game.play(2,1);
+	assertEquals(1, game.getTies());
     }
 }
