f109e663eabd21fbc8662942ce58891226793dc0
Freyr Bergsteinsson <fresnik@gmail.com>
2014-10-27 21:29:48 +0000

    Web API Implementation

--- src/main/java/is/ru/geitibandi/geitimyllu/GeitIMylluWeb.java
+++ src/main/java/is/ru/geitibandi/geitimyllu/GeitIMylluWeb.java
@@ -6,6 +6,8 @@ import spark.servlet.SparkApplication;
 
 // Class that handles incoming web requests and sends RESTful responses in return
 public class GeitIMylluWeb implements SparkApplication {
+    private final static String apiUrl = "/api/v1";
+
     public static void main(String[] args){
         SparkApplication webapp = new GeitIMylluWeb();
         webapp.init();
@@ -14,16 +16,16 @@ public class GeitIMylluWeb implements SparkApplication {
     public void init(){
         final GeitIMyllu game = new GeitIMyllu();
 
-        get(new Route("/api/v1/turn"){
+        get(new Route(apiUrl + "/state") {
             @Override
-            public Object handle(Request request, Response response){
-                return game.getTurn();
+            public Object handle(Request request, Response response) {
+                return game.getGameState();
             }
         });
 
-        post(new Route("/api/v1/play/:row/:col"){
+        post(new Route(apiUrl + "/play/:row/:col") {
             @Override
-            public Object handle(Request request, Response response){
+            public Object handle(Request request, Response response) {
                 try {
                     game.play(Integer.parseInt(":row"), Integer.parseInt(":col"));
                 }
@@ -32,6 +34,72 @@ public class GeitIMylluWeb implements SparkApplication {
                     response.body(e.getMessage());
                 }
                 return "";
+            }
+        });
+
+        post(new Route(apiUrl + "/reset") {
+            @Override
+            public Object handle(Request request, Response response) {
+                game.resetGame();
+                return "";
+            }
+        });
+
+        get(new Route(apiUrl + "/board") {
+            @Override
+            public Object handle(Request request, Response response) {
+                return game.boardAsString();
+            }
+        });
+
+        get(new Route(apiUrl + "/turn") {
+            @Override
+            public Object handle(Request request, Response response) {
+                return game.getTurn();
+            }
+        });
+
+        get(new Route(apiUrl + "/whosturn") {
+            public Object handle(Request request, Response response) {
+                return game.getCurrentPlayer();
+            }
+        });
+
+        get(new Route(apiUrl + "/played") {
+            public Object handle(Request request, Response response) {
+                return game.getPlayedGames();
+            }
+        });
+
+        get(new Route(apiUrl + "/won") {
+            public Object handle(Request request, Response response) {
+                boolean gameWon = game.gameWon();
+                boolean boardFull = game.boardFull();
+                char winner = (gameWon ? game.getCurrentPlayer() :
+                    (boardFull ? 'T' : 'N'));
+                return winner;
+            }
+        });
+
+        get(new Route(apiUrl + "/won/:player") {
+            public Object handle(Request request, Response response) {
+                String playerStr = ":player";
+                char player = playerStr.charAt(0);
+                if (player == 'X' || player == 'x') {
+                    return game.getGamesWonByPlayerX();
+                }
+                else if (player == 'O' || player == 'o') {
+                    return game.getGamesWonByPlayerO();
+                }
+                else {
+                    throw new IllegalArgumentException("Player must be X or O.");
+                }
+            }
+        });
+
+        get(new Route(apiUrl + "/ties") {
+            public Object handle(Request request, Response response) {
+                return game.getTies();
             }
         });
     }
