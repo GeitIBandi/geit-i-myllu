31bdfaa0f0206c2c9922abf2f23796a140205f10
Freyr Bergsteinsson <freyrb12@ru.is>
2014-11-01 23:27:56 +0000

    Moving deployment steps into script file

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit #abort if any command fails
 
-git checkout production
-git merge --no-edit -s ours master
-git push origin production
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+git remote add heroku-staging git@heroku.com:geit-i-myllu-staging.git
+echo "Host heroku.com" >> ~/.ssh/config
+echo "   StrictHostKeyChecking no" >> ~/.ssh/config
+echo "   CheckHostIP no" >> ~/.ssh/config
+echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
+yes | heroku keys:add
+git push heroku-staging master
