851a8e27527f3a1bfa4fd039fd55a2112cdf7def
Freyr Bergsteinsson <freyrb12@ru.is>
2014-11-02 02:00:39 +0000

    Use github oauth token to publish code coverage reports

--- bin/deploy-code-coverage.sh
+++ bin/deploy-code-coverage.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit #abort if any command fails
 
-# Temporary - until fix found
-exit 0
-
 rm -fr /tmp/reports
 rm -fr /tmp/geit-i-myllu
 cp -a build/reports /tmp
 cd /tmp
-git clone git@github.com:GeitIBandi/geit-i-myllu.git -b gh-pages
+git clone https://171f9bef31b10bcacc8960b582aff056d3258274@github.com:GeitIBandi/geit-i-myllu.git -b gh-pages
 cd geit-i-myllu
 mkdir -p reports
 mv /tmp/reports/* reports
