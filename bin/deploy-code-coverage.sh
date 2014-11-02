a97e72c355186200791525f48c7b680aacff418b
Freyr Bergsteinsson <freyrb12@ru.is>
2014-11-02 02:12:00 +0000

    Fix code coverage deployment script

--- bin/deploy-code-coverage.sh
+++ bin/deploy-code-coverage.sh
@@ -7,8 +7,8 @@ cp -a build/reports /tmp
 cd /tmp
 git clone https://171f9bef31b10bcacc8960b582aff056d3258274@github.com/GeitIBandi/geit-i-myllu.git -b gh-pages
 cd geit-i-myllu
-mkdir -p reports
-mv /tmp/reports/* reports
+rm -fr reports
+mv /tmp/reports .
 git add reports
 git commit -m "Automatic updates or reports"
 git push origin gh-pages
