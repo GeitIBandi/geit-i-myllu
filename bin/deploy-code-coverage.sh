4be41c4974e9726d8980e2ea61765ce9972b530e
Freyr Bergsteinsson <freyrb12@ru.is>
2014-11-02 01:17:27 +0000

    Automatically uploade code coverage reports to gh-pages

---
+++ bin/deploy-code-coverage.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -o errexit #abort if any command fails
+
+rm -fr /tmp/reports
+rm -fr /tmp/geit-i-myllu
+cp -a build/reports /tmp
+cd /tmp
+git clone git@github.com:GeitIBandi/geit-i-myllu.git -b gh-pages
+cd geit-i-myllu
+mkdir -p reports
+mv /tmp/reports/* reports
+git add reports
+git commit -m "Automatic updates or reports"
+git push origin gh-pages
